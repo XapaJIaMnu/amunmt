b51bed07247091c0f208cd211b995bdc59bd9ff4
Nikolay Bogoychev <nheart@gmail.com>
2017-04-28 17:08:50 +0100

    Batched neural network training

--- dropStates/NN2.py
+++ dropStates/NN2.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import tensorflow as tf
 import numpy
 from sklearn.model_selection import train_test_split
@@ -10,33 +11,36 @@ def init_weight(shape, name):
 
 def forwardpass(X, X_ID, w_1, b_1):
     """Forward pass of the NN"""
+    # Instead of doing full matrix multiplication, just consider the vocabulary IDs
+    # that are in this batch
     selected_embeddings_w_1 = tf.gather(w_1, X_ID)
     selected_embeddings_b_1 = tf.gather(b_1, X_ID)
 
+    # Transpose the matrices to their traditional representation
     selected_embeddings_w_1_t = tf.transpose(selected_embeddings_w_1, name="W1_T")
     selected_embeddings_b_1_t = tf.transpose(selected_embeddings_b_1, name="B1_T")
-    print(selected_embeddings_w_1_t)
-    print(selected_embeddings_b_1_t)
+    
+    # Multiply with X with W_1
     multiplication = tf.matmul(X, selected_embeddings_w_1_t, name="X_W1_T")
-    print(multiplication)
-    bias_addition = tf.add(multiplication, selected_embeddings_b_1_t, name="X_W1_T_B1_T")
-    #We are only interested in the first column, so we transpose and gather
-    bias_first_col = tf.gather(tf.transpose(bias_addition), 1, name="Y_HAT")
-    print(bias_first_col)
-    return bias_first_col
-    #multiplication = tf.add(tf.matmul(X, w_1), b_1)
-    #return tf.gather(tf.transpose(multiplication), X_ID)
-    #return tf.gather_nd(multiplication, [X_ID, 1])
 
-def FFNN_train(data):
+    # We want the first column here, because every vector of X corresponds to a single
+    # vocabID and not each vector with all vocabIDs in the batch
+    mult_first_column = tf.gather(tf.transpose(multiplication), 1, name="Y_column_1")
+
+    # add the bias
+    bias_addition = tf.add(mult_first_column, selected_embeddings_b_1_t, name="Y_hat")
+    return bias_addition
+
+
+def FFNN_train(data, hidden_layer, vocab):
     """Defines the feed forward neural network"""
-    x_size = 500 # Size of the hidden layer input
-    y_size = 90321 # Vocab size
+    x_size = hidden_layer # Size of the hidden layer input
+    y_size = vocab # Vocab size
 
     # tf Graph Input
-    X = tf.placeholder("float", name="X", shape=[32, x_size])
-    X_ID = tf.placeholder("int32", name="X_ID", shape=[32])
-    Y = tf.placeholder("float", name="Y", shape=[32])
+    X = tf.placeholder("float", name="X", shape=[None, x_size])
+    X_ID = tf.placeholder("int32", name="X_ID", shape=[None])
+    Y = tf.placeholder("float", name="Y", shape=[None])
 
     # init weights
     w_1 = init_weight((y_size, x_size), 'w1') # Swapped around because tf.gather() is stupid
@@ -44,11 +48,11 @@ def FFNN_train(data):
 
     # Forward pass
     y_hat = forwardpass(X, X_ID, w_1, b_1)
-    print(Y)
-    print(y_hat)
+
+    # Error
     cost = tf.reduce_sum(tf.pow((Y - y_hat), 2))
 
-    #Use adam to optimize and initialize the cost
+    # Use adam to optimize and initialize the cost
     train_op = tf.train.AdamOptimizer(0.001).minimize(cost)
     init_op = tf.global_variables_initializer()
 
@@ -60,7 +64,7 @@ def FFNN_train(data):
         sess.run(init_op)
 
         # Fit all training data
-        for epoch in range(10):
+        for epoch in range(100):
             sess.run(train_op, feed_dict={X: x_vec_train, X_ID: x_id_train, Y: y_train})
             #for (x_vec, x_id, y) in zip(x_vec_train, x_id_train, y_train):
             #    sess.run(train_op, feed_dict={X: x_vec.reshape(1,500), X_ID: x_id.reshape(1), Y: y.reshape(1)})#, options=run_options, run_metadata=run_metadata)
@@ -72,8 +76,29 @@ def FFNN_train(data):
             #    f.write(ctf)
 
             # Display logs per epoch step
-            #c = sess.run(cost, feed_dict={X: x_vec.reshape(1,500), X_ID: x_id.reshape(1), Y: y.reshape(1)})
-            #print("Epoch:" + str(epoch + 1) + " cost= " + str(c))
+            c = sess.run(cost, feed_dict={X: x_vec_test, X_ID: x_id_test, Y: y_test})
+            print("Epoch:" + str(epoch + 1) + " cost= " + str(c))
+        return (w_1.eval(), b_1.eval())
+
+def get_dataset_error(data, w1, b1):
+    # Load data
+    X, X_ID, Y = tf.constant(data[0]), tf.constant(data[1]), tf.constant(data[2])
+
+    # Load the model
+    w_1 = tf.constant(w1)
+    b_1 = tf.constant(b1)
+
+    # Compute predictions
+    y_hat = forwardpass(X, X_ID, w_1, b_1)
+    cost = tf.reduce_sum(tf.pow((Y - y_hat), 2))
+
+    # Init vars
+    init_op = tf.global_variables_initializer()
+
+    with tf.Session() as sess:
+        sess.run(init_op)
+        c = sess.run(cost)
+        print("Total error: " + str(c))
 
 def preprocess(filename):
     """Creates the dataset"""
@@ -90,3 +115,7 @@ def preprocess(filename):
         X_vec.append([float(x) for x in vec.strip().split(' ')])
     train_file.close()
     return (numpy.array(X_vec).astype('float32'), numpy.array(X_wID).astype("int32"), numpy.array(Y).astype('float32'))
+
+def train_NN(filename, hidden_layer, vocab):
+    data = preprocess(filename)
+    return FFNN_train(data, hidden_layer, vocab)
